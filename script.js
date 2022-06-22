c514cccccf4e5d321faf7720660475274c4138cc
Akinuliola Akinbobola <akinuliola_a@yahoo.com>
2022-06-22 22:58:47 +0100

    update

---
+++ script.js
@@ -0,0 +1,15 @@
+alert("what type of arichmetic operation do you want to perform?");
+var operation = prompt("+, -, *, /");
+var num1 = prompt("enter first number");
+var num2 = prompt("enter second number");
+var result = 0;
+if (operation === "+") {
+  result = parseInt(num1) + parseInt(num2);
+} else if (operation === "-") {
+  result = parseInt(num1) - parseInt(num2);
+} else if (operation === "*") {
+  result = parseInt(num1) * parseInt(num2);
+} else if (operation === "/") {
+  result = parseInt(num1) / parseInt(num2);
+}
+alert(result);
